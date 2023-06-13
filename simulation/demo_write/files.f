9e0d170be30ce2ecc3b727f95af360767590941a
uty <whensungoes@gmail.com>
2023-06-13 22:23:37 +0800

    demo_write files.f update

--- simulation/demo_write/files.f
+++ simulation/demo_write/files.f
@@ -1,6 +1,8 @@
-/home/u/altera/13.1/quartus/eda/sim_lib/altera_mf.v
+#/home/u/altera/13.1/quartus/eda/sim_lib/altera_mf.v
+../altera_mf.v
 ../../rtl/defines.vh
 ../../rtl/axi_sram_bridge.v
+../../rtl/axi_interface.v
 ../../rtl/swrvr_clib.v 
 ../../rtl/swrvr_dlib.v
 ../../rtl/defines.vh 
